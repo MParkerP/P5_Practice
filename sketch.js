bac0f1a7b4b6577ef26307047218595a077f71ae
Matthew Posey <mparkerposey@outlook.com>
2024-01-21 20:08:19 -0600

    began work up to exmaple 2

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(365, 1000);
 }
 
 function draw() {
-  background("green");
+  //EXAMPLE 1
+
+  //background
+  strokeWeight(0);
+  fill(91, 240, 86);
+  rect(0,0,365,180);
+  
+  //circle
+  strokeWeight(2);
+  fill('white');
+  circle(90,90,150);
+  
+  //square
+  square(200, 15, 150);
+
+
+
+  //EXAMPLE 2
+  
+  //background
+  strokeWeight(1);
+  fill('white');
+  rect(0,180,365,250);
+
+  //red circle
+  circle(180,275,150);
+  //blue circle
+
+  //green circle
 }
